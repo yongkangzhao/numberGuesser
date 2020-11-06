06830e01620d24cd8a5d51862b9878f22c78a2ad
Yongkang Zhao <cookieyong@gmail.com>
2020-11-06 15:41:04 -0800

    implemented the guessing logic, implemented play again feature, implemented color changing feature

---
+++ app.js
@@ -0,0 +1,69 @@
+let min;
+let max;
+let answer;
+let defaultLives;
+let currentLives;
+
+
+
+document.addEventListener('DOMContentLoaded', initialize);
+document.getElementById('guess-value').addEventListener('click', evaluate);
+const submit = document.querySelector('#guess-value');
+const guess = document.getElementById('guess-input');
+const msg = document.querySelector('.message');
+function initialize(e){
+    min = 1;
+    max = 10;
+    answer = Math.round(Math.random() * (max - min) + min);
+    defaultLives = 3;
+    currentLives = defaultLives;
+    submit.value = 'SUBMIT';
+    msg.innerHTML = '';
+    guess.value = '';
+    msg.className = '';
+    console.log(answer);
+}
+
+
+function evaluate(e){
+    if (guess.value == ''){
+        alert('Enter your guess');
+        return;
+    }
+    if (submit.value === 'Play Again!'){
+        initialize();
+        return;
+    }
+
+    if (guess.value == answer){
+        // correct
+        correct();
+        
+    }else{
+        // wrong
+        wrong();
+    }
+}
+
+function correct(){
+    msg.innerHTML = 'correct';
+    msg.className = 'correct';
+    gameOver();
+}
+
+function wrong(){
+    msg.className = 'wrong';
+    currentLives -= 1;
+    if (currentLives == 0){
+        msg.innerHTML = 'wrong and the answer is: '+answer;
+        gameOver();
+    }else{
+        msg.innerHTML = 'wrong and you have ' + currentLives +' lives left';
+    }
+}
+
+function gameOver(){
+    submit.value = 'Play Again!';
+}
+
+
